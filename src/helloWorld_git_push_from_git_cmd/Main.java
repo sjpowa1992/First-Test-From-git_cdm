953a722b4f1d6138aaaa26cbe344858c4722786b
Iovino <sergio.iovino@accenture.com>
2022-04-13 12:45:23 +0200

    first commit

---
+++ src/helloWorld_git_push_from_git_cmd/Main.java
@@ -0,0 +1,10 @@
+package helloWorld_git_push_from_git_cmd;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello sjpowa :)"
+				+ "\nHave a nice day!");
+	}
+	
+}
